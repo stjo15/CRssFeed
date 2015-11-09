6d21de1cd35a115f56b01eb530b14bd7d70a1971
Staffan Johansson <stalle.johansson@gmail.com>
2015-11-09 20:07:06 +0100

    fixed bugs

--- test/config.php
+++ test/config.php
@@ -6,5 +6,5 @@ include __DIR__ . "/../../../autoload.php";
  * Define essential Anax paths, end with /
  *
  */
-define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../../../..') . '/');
+define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../vendor/anax/mvc') . '/');
 define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . 'app/');
