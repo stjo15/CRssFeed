8cd1bb157f266389fbf1b6a9da554c6994dd61db
Staffan Johansson <stalle.johansson@gmail.com>
2015-11-10 12:00:09 +0100

    changed config and autoloader files

--- webroot/config_with_app.php
+++ webroot/config_with_app.php
@@ -5,7 +5,7 @@
  */
 
 // Get environment & autoloader.
-require __DIR__.'/config.php'; 
+require ANAX_INSTALL_PATH .'webroot/config.php'; 
 
 // Create services and inject into the app. 
 $di  = new \Anax\DI\CDIFactoryDefault();
