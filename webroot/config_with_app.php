22c22e8f9eb4a50c5bc1dc330b5deeaa43f170c8
Staffan Johansson <stalle.johansson@gmail.com>
2015-11-10 14:23:40 +0100

    fixed even more bugs...

--- webroot/config_with_app.php
+++ webroot/config_with_app.php
@@ -5,7 +5,7 @@
  */
 
 // Get environment & autoloader.
-require ANAX_INSTALL_PATH .'webroot/config.php'; 
+require ANAX_PATH .'webroot/config.php'; 
 
 // Create services and inject into the app. 
 $di  = new \Anax\DI\CDIFactoryDefault();
